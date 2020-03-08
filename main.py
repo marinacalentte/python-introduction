b7476f21dd4cc7066857a3bc541f08a8f636f84c
marinacalentte <calentte.marina@gmail.com>
2020-03-08 21:43:17 +0000

    Calcular idade

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+from datetime import date
+
 def introducao_pessoal(nome, idade):
   print("ola, eu sou ", nome, " e tenho ", idade, " anos")
+
+def calcular_idade(data_nascimento):
+  today = date.today()
+  return today.year - data_nascimento.year - ((today.month, today.day) < (data_nascimento.month, data_nascimento.day))
 
 introducao_pessoal("andre", 37)
 introducao_pessoal("marina", 35)
 introducao_pessoal("bento", 4)
+
+print(calcular_idade(date(1982, 7, 6)))
