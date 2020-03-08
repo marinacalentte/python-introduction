6a1377c112bfaa378c2553d6c5f3e3758f9ed415
marinacalentte <calentte.marina@gmail.com>
2020-03-08 21:35:17 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+def introducao_pessoal(nome, idade):
+  print("ola, eu sou ", nome, " e tenho ", idade, " anos")
+
+introducao_pessoal("andre", 37)
+introducao_pessoal("marina", 35)
+introducao_pessoal("bento", 4)
